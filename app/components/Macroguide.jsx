b0e6e73aff9d7bb7e9c9065d68390957c96db086
Nathaniel Quashie <nathanielcfa@gmail.com>
2016-09-07 14:08:43 +0000

    Add - styles to macroguide component

--- app/components/Macroguide.jsx
+++ app/components/Macroguide.jsx
@@ -3,9 +3,11 @@ var React = require('react');
 var Macroguide = React.createClass({
   render: function(){
     return (
-      <div>
-        <h1 className="page-title">Nutrition Notes</h1>
-        <p>I will aim for an average, fat loss rate of 0.6-1.0lbs per week.</p>
+      <div className="row">
+        <div className="columns medium-6 large-4 small-centered">
+          <h1 className="page-title">Nutrition Notes</h1>
+          <p>I will aim for an average, fat loss rate of 0.6-1.0lbs per week.</p>
+        </div>
       </div>
     )
   }
