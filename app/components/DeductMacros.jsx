fd6104ee8803651934995be2e7d420259e4fd272
bklynate <nathanielcfa@gmail.com>
2016-09-08 20:26:53 -0400

    Add - more input validations

--- app/components/DeductMacros.jsx
+++ app/components/DeductMacros.jsx
@@ -6,7 +6,7 @@ var DeductMacros = React.createClass({
 
     var macrograms = this.refs.macrograms.value;
 
-    if(macrograms.match(/^[0-9]*$/)){
+    if(macrograms.match(/^[0-9]*$/) && macrograms !== ""){
       this.refs.macrograms.value = "";
       this.props.onMacroSub(parseInt(macrograms, 10));
     }
